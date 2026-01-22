b2b83c23eb4929884014391cf005a25bf084aac3
Khujesta1998 <Khujesta.Rizayee@edu.sait.ca>
2026-01-22 15:45:22 -0700

    week 2 assignment

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,9 @@
+export default function studentInfo() {
+  return (
+    <div>
+      <h2>Name: Khujesta Rizayee</h2>
+      <h3><a href="https://github.com/Khujesta1998/cprg306-assignments"> GitHub: Khujesta1998 </a></h3>
+    </div>
+  );
+}
+ 
